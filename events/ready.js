a8af8b3c81550b9545234b564829c210a0f21350
F-14-Tomcat <79108893+F-14-Tomcat@users.noreply.github.com>
2023-08-14 22:07:31 -0400

    Added Footer, Added Custom Status, No Servers Bug Fix (#5)
    * Removed bug that would throw an error if no server information could be displayed. (#3)
    
    * Fixed bug where setting a filter that shows zero filter would throw an error
    
    * Added description to embed when zero servers are shown
    
    * Added footer, Added Custom Status (#4)
    
    * Added footer and removed unnecessary console.log from servers commands
    
    * Updated to discord.js v14.13 (dev build), added custom status
    
    * Fixed bug resulting from showing zero servers
    
    * Removed unnecessary directory from .gitignore
    
    * Bot status now shows number of servers, number of players, most popular region, removed updating status from join and leave

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,45 @@ const { Events, ActivityType } = require('discord.js');
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
-        client.user.setActivity(`Battlebit on ${client.guilds.cache.size} Discord Servers`, { type: ActivityType.Playing });
+    async execute(client) {
+        setInterval(updateActivity, 5000, client);
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
+};
+
+async function updateActivity(client) {
+    let servers = await retrieveServerData();
+    client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `BattleBit Servers: ${servers.length.toString()} | Players in Game: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} | Most Popular Region: ${getMostPopular(servers, 'Region')}`});
+}
+
+function retrieveServerData() {
+    const url = "https://publicapi.battlebit.cloud/Servers/GetServerList";
+    return fetch(url).then(response => {
+        return response.text().then(servers => {
+            if (servers) {
+                return JSON.parse(servers.trim());
+            }
+        })
+        .catch(error => {
+            interaction.reply("error");
+            console.error("Error occurred while retrieving API data:", error.message);
+        });
+    });
+}
+
+function getMostPopular(servers, property) {
+    let list = [];
+    for(server of servers){
+        let name = server[property];
+        let current = list.find(item => item.name == name);
+        if(current  !== undefined){
+            current.servers++;
+            current.players += server.Players;
+        }else{
+            current = {name: name, servers: 1, players: server.Players};
+            list.push(current);
+        }
+    }
+    list.sort(function(a, b){return b.players - a.players});
+    return list[0].name;
 };
