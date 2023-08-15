a5ed209af5cdaa34ec05b3bbd86555fd5f6d06eb
F-14-Tomcat <79108893+F-14-Tomcat@users.noreply.github.com>
2023-08-14 21:09:28 -0400

    Updated to discord.js v14.13 (dev build), added custom status

--- events/leave.js
+++ events/leave.js
@@ -6,7 +6,7 @@ module.exports = {
     name: Events.GuildDelete,
     execute(guild) {
 	const client = guild.client;
-	client.user.setActivity(`Battlebit on ${client.guilds.cache.size} Discord Servers`, { type: ActivityType.Playing });
+	client.user.setActivity(`Battlebit on ${client.guilds.cache.size} Discord Servers`, { type: ActivityType.Custom, state: 'Testing' });
 	console.log(`Currently in ${client.guilds.cache.size} servers`);
 	client.channels.fetch(channelId).then(channel => channel.send(`Left ${guild.name}\nNow in ${client.guilds.cache.size} Discord Servers`));
     },
--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,34 @@
 const { Events, ActivityType } = require('discord.js');
+const axios = require('axios');
+const { token, version } = require('../config.json');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        client.user.setActivity(`Battlebit on ${client.guilds.cache.size} Discord Servers`, { type: ActivityType.Playing });
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+        let servers = [];
+        retrieveApiData()
+            .then(apiData => {
+                if (apiData) {
+                    servers = JSON.parse(apiData.trim());
+                    setActivity(client, servers);
+                }
+            })
+            .catch(error => {
+                interaction.reply("error");
+                console.error("Error occurred while retrieving API data:", error.message);
+            });
     },
 };
+
+function retrieveApiData() {
+    const url = "https://publicapi.battlebit.cloud/Servers/GetServerList";
+    return fetch(url).then(response => {
+        return response.text();
+    });
+}
+
+function setActivity(client, servers) {
+    client.user.setActivity('test', { type: 4, state: `BattleBit Servers: ${servers.length.toString()}\tPlayers in Game: ${servers.reduce((n, {Players}) => n + Players, 0).toString()}`});
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+}
