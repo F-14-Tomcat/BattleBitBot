f4dbd663e23580032144197fe7bc1ded28d21f74
F-14-Tomcat <79108893+F-14-Tomcat@users.noreply.github.com>
2023-08-15 14:56:48 -0400

    Rotates status to show different regions

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,40 @@
 const { Events, ActivityType } = require('discord.js');
+let index = 0;
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     async execute(client) {
-        setInterval(updateActivity, 3000, client);
+        setInterval(updateActivity, 5000, client);
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
 
 async function updateActivity(client) {
     let servers = await retrieveServerData();
-    client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Players in Game: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} | Players in Queue: ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} | BattleBit Servers: ${servers.length.toString()} | Most Popular Region: ${getMostPopular(servers, 'Region')}`});
+    switch(index % 5){
+        case 0:
+            servers = servers.filter(server => server.Region == 'America_Central');
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `America: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            break;
+        case 1:
+            servers = servers.filter(server => server.Region == 'Europe_Central');
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Europe: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            break;
+        case 2:
+            servers = servers.filter(server => server.Region == 'Japan_Central');
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Japan: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            break;
+        case 3:
+            servers = servers.filter(server => server.Region == 'Australia_Central');
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Australia: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            break;
+        case 4:
+            servers = servers.filter(server => server.Region == 'Brazil_Central');
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Brazil: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            break;
+    }
+    index++;
 }
 
 function retrieveServerData() {
