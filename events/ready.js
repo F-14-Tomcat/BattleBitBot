6a3a0867600e4c3e4371287fc501687bdd2dc034
F-14-Tomcat <79108893+F-14-Tomcat@users.noreply.github.com>
2023-08-16 13:43:26 -0400

    Development (#15)
    * Added startup delay of 1 second to test pm2 on deployment
    
    * Fixed bug that would add an s to the end even when only 1 player/sever was present (#11)
    
    * Fixed bug that would crash the bot when the serverList could not be fetched (#12)
    
    * Removed the code that selected the most popular 'x' because it is no longer necessary (#13)
    
    * Added startup delay of 1 second to test pm2 on deployment (#14)

--- events/ready.js
+++ events/ready.js
@@ -15,23 +15,23 @@ async function updateActivity(client) {
     switch(index % 5){
         case 0:
             servers = servers.filter(server => server.Region == 'America_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `America: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `America: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 1:
             servers = servers.filter(server => server.Region == 'Europe_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Europe: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Europe: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 2:
             servers = servers.filter(server => server.Region == 'Japan_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Japan: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Japan: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 3:
             servers = servers.filter(server => server.Region == 'Australia_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Australia: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Australia: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 4:
             servers = servers.filter(server => server.Region == 'Brazil_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Brazil: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Brazil: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
     }
     index++;
@@ -46,25 +46,7 @@ function retrieveServerData() {
             }
         })
         .catch(error => {
-            interaction.reply("error");
-            console.error("Error occurred while retrieving API data:", error.message);
+            console.error("Error occurred while retrieving API data in ready.js:", error.message);
         });
     });
 }
-
-function getMostPopular(servers, property) {
-    let list = [];
-    for(server of servers){
-        let name = server[property];
-        let current = list.find(item => item.name == name);
-        if(current  !== undefined){
-            current.servers++;
-            current.players += server.Players;
-        }else{
-            current = {name: name, servers: 1, players: server.Players};
-            list.push(current);
-        }
-    }
-    list.sort(function(a, b){return b.players - a.players});
-    return list[0].name;
-};
