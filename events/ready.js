effc5e15ee72bb2b8d54776f80a0811422528044
F-14-Tomcat <79108893+F-14-Tomcat@users.noreply.github.com>
2023-08-16 13:40:23 -0400

    Fixed bug that would add an s to the end even when only 1 player/sever was present (#11)

--- events/ready.js
+++ events/ready.js
@@ -15,23 +15,23 @@ async function updateActivity(client) {
     switch(index % 5){
         case 0:
             servers = servers.filter(server => server.Region == 'America_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `America: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `America: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 1:
             servers = servers.filter(server => server.Region == 'Europe_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Europe: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Europe: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 2:
             servers = servers.filter(server => server.Region == 'Japan_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Japan: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Japan: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 3:
             servers = servers.filter(server => server.Region == 'Australia_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Australia: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Australia: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
         case 4:
             servers = servers.filter(server => server.Region == 'Brazil_Central');
-            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Brazil: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Players | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Players in Queue | ${servers.length.toString()} Servers`});
+            client.user.setActivity({ name: 'Name', type: ActivityType.Custom, state: `Brazil: ${servers.reduce((n, {Players}) => n + Players, 0).toString()} Player${servers.length == 1 ? '' : 's'} | ${servers.reduce((n, {QueuePlayers}) => n + QueuePlayers, 0).toString()} Player${servers.length == 1 ? '' : 's'} in Queue | ${servers.length.toString()} Server${servers.length == 1 ? '' : 's'}`});
             break;
     }
     index++;
