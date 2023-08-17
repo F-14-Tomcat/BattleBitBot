23d9bfc3cff460c5f6674992f8214d389867bec1
F-14-Tomcat <79108893+F-14-Tomcat@users.noreply.github.com>
2023-08-17 14:04:27 -0400

    Add image to embed (#18)
    * Added hyperlink to invite in embed. Made all Fields in-line
    
    * Added maps and flags directories, added embed image

--- commands/global/server.js
+++ commands/global/server.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const fetch = require('node-fetch');
+const { createCanvas, loadImage } = require('canvas');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -72,25 +73,34 @@ module.exports = {
         ),
 
     async execute(interaction) {
+        //interaction.deferReply({ ephemeral: true });
         let servers = await retrieveServerData();
         let description = '';
+        let region = '';
+        let map = '';
+        let gamemode = '';
+        let maxplayers = '';
 
         if(interaction.options.getString('region')){
+            region = interaction.options.getString('region');
             description += '\nRegion: ' + interaction.options.getString('region');
             servers = servers.filter(server => server.Region === interaction.options.getString('region'));
         }
 
         if(interaction.options.getString('map')){
+            map = interaction.options.getString('map')
             description += '\nMap: ' + interaction.options.getString('map');
             servers = servers.filter(server => server.Map === interaction.options.getString('map'));
         }
 
         if(interaction.options.getString('gamemode')){
+            gamemode = interaction.options.getString('gamemode');
             description += '\nGamemode: ' + interaction.options.getString('gamemode');
             servers = servers.filter(server => server.Gamemode === interaction.options.getString('gamemode'));
         }
 
         if(interaction.options.getString('maxplayers')){
+            maxplayers = interaction.options.getString('maxplayers');
             if(interaction.options.getString('maxplayers') === 'Other'){
                 servers = servers.filter(server => (server.MaxPlayers !== 254 && server.MaxPlayers !== 128 && server.MaxPlayers !== 64 && server.MaxPlayers !== 32));
             }else{
@@ -112,25 +122,37 @@ module.exports = {
             newEmbed.setDescription(description);
         }
 
-        if(!interaction.options.getString('region')){
-            newEmbed.addFields({ name: 'Most Popular Region', value: getPopular(servers, 'Region'), inline: true})
+        if(servers.length > 0){
+            newEmbed.setFooter({ text: `BattleBit Version: ${servers[0].Build}` })
         }
 
-        if(!interaction.options.getString('map')){
-            newEmbed.addFields({ name: 'Most Popular Map', value: getPopular(servers, 'Map'), inline: true})
+        if(servers.length > 0 && !interaction.options.getString('region')){
+            newEmbed.addFields({ name: 'Most Popular Region', value: makeList(getPopular(servers, 'Region')), inline: true})
+            region = getPopular(servers, 'Region')[0].name;
         }
 
-        if(!interaction.options.getString('gamemode')){
-            newEmbed.addFields({ name: 'Most Popular Gamemode', value: getPopular(servers, 'Gamemode'), inline: true})
+        if(servers.length > 0 && !interaction.options.getString('map')){
+            newEmbed.addFields({ name: 'Most Popular Map', value: makeList(getPopular(servers, 'Map')), inline: true})
+            map = getPopular(servers, 'Map')[0].name;
         }
 
-        if(!interaction.options.getString('maxplayers')){
-            newEmbed.addFields({ name: 'Most Popular Server Size', value: getPopular(servers, 'MaxPlayers'), inline: true})
+        if(servers.length > 0 && !interaction.options.getString('gamemode')){
+            newEmbed.addFields({ name: 'Most Popular Gamemode', value: makeList(getPopular(servers, 'Gamemode')), inline: true})
+            gamemode = getPopular(servers, 'Gamemode')[0].name;
+        }
+
+        if(servers.length > 0 && !interaction.options.getString('maxplayers')){
+            newEmbed.addFields({ name: 'Most Popular Server Size', value: makeList(getPopular(servers, 'MaxPlayers')), inline: true})
+            maxplayers = getPopular(servers, 'MaxPlayers')[0].name;
         }
 
         newEmbed.addFields({ name: '\u200b', value: 'Click [HERE](https://discord.com/api/oauth2/authorize?client_id=1139370092547809372&permissions=0&scope=applications.commands%20bot) to add the bot to your server.'});
-
-        return interaction.reply({ embeds: [newEmbed] });
+        const file = new AttachmentBuilder(await generateImage(region, map, gamemode, maxplayers), { name: 'image.png' });
+        newEmbed.setThumbnail('attachment://image.png');
+        if(interaction.deferred){
+            return interaction.editReply({ embeds: [newEmbed], files: [file] });
+        }
+        return interaction.reply({ embeds: [newEmbed], files: [file] });
     },
 };
 
@@ -143,7 +165,7 @@ function retrieveServerData() {
             }
         })
         .catch(error => {
-            interaction.reply("error");
+            interaction.editReply("error", { ephemeral: true });
             console.error("Error occurred while retrieving API data:", error.message);
         });
     });
@@ -162,10 +184,64 @@ function getPopular(servers, property) {
             list.push(current);
         }
     }
-    list.sort(function(a, b){return b.players - a.players});
+    return list.sort(function(a, b){return b.players - a.players});
+};
+
+function makeList(list) {
     let returnString = '';
     for(let i = 0; i < Math.min(3, list.length); i++){
         returnString += `${i + 1}. ${list[i].name}: ${list[i].players} players on ${list[i].servers} servers\n`;
     }
     return returnString;
-};
+}
+
+async function generateImage(region, map, gamemode, maxplayers) {
+    const imageHeight = 256;
+    const canvas = createCanvas(imageHeight, imageHeight);
+    const ctx = canvas.getContext('2d');
+    switch(gamemode){
+        case 'CONQ':
+            gamemode = 'CONQUEST';
+            break;
+        case 'DOMI':
+            gamemode = 'DOMINATION';
+            break;
+        case 'ELI':
+            gamemode = 'ELIMINATION';
+            break;
+    }
+    switch(maxplayers.toString()){
+        case '254':
+            maxplayers = '127v127';
+            break;
+        case '128':
+            maxplayers = '64v64';
+            break;
+        case '64':
+            maxplayers = '32v32';
+            break;
+        case '32':
+            maxplayers = '16v16';
+            break;
+        default:
+            maxplayers = '';
+            break;
+    }
+    await loadImage(`./maps/${map}.png`).then((image) => {
+        canvas.width = imageHeight * (image.width / image.height);
+        canvas.height = imageHeight;
+        ctx.drawImage(image, 0, 0, imageHeight * (image.width / image.height), imageHeight);
+    });
+    await loadImage(`./flags/${region}.png`).then((image) => {
+        ctx.drawImage(image, 10, 10, (image.width / image.height) * (canvas.width/10) + 10, (canvas.width/10) + 10);
+    });
+    ctx.font = `${Math.ceil(imageHeight/8)}px Impact`
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = Math.ceil(imageHeight/128);
+    ctx.strokeText(gamemode, (canvas.width / 2), 10 + Math.ceil(imageHeight/8));
+    ctx.strokeText(maxplayers, (canvas.width / 2), 2 * (10 + Math.ceil(imageHeight/8)));
+    ctx.fillStyle = 'white';
+    ctx.fillText(gamemode, (canvas.width / 2), 10 + Math.ceil(imageHeight/8));
+    ctx.fillText(maxplayers, (canvas.width / 2), 2 * (10 + Math.ceil(imageHeight/8)));
+    return canvas.createPNGStream();
+}
